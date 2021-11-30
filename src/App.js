20a7b99460e4de0b1cb52b70acd596cd4e02139e
erikwillemse <47789869+erikwillemse@users.noreply.github.com>
2021-11-30 09:45:27 +0100

    Lots of changes and updates

--- src/App.js
+++ src/App.js
@@ -1,74 +1,161 @@
 import algoliasearch from 'algoliasearch/lite';
-import React, { Component } from 'react';
+import React from 'react';
 import {
   InstantSearch,
+  Index,
   Hits,
+  Configure,
   SearchBox,
   Pagination,
   Highlight,
   HierarchicalMenu,
 } from 'react-instantsearch-dom';
-import PropTypes from 'prop-types';
 import './App.css';
+
+import Card from '@mui/material/Card';
+import CardMedia from '@mui/material/CardMedia';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Button from '@mui/material/Button';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
 
 const searchClient = algoliasearch(
   'CSDBX0SZMQ',
   '4bfa904cde10c4036e72bb5ad6a698d4'
 );
 
-class App extends Component {
-  render() {
-    return (
-      <div className="ais-InstantSearch">
-        <h1>React InstantSearch e-commerce demo</h1>
-        <InstantSearch
-          indexName="crawler_tg_content"
-          searchClient={searchClient}
-        >
-          <div className="left-panel">
-            <h2>Categorieën</h2>
-            <HierarchicalMenu
-              attributes={['categories.lvl0', 'categories.lvl1']}
-            />
-          </div>
-          <div className="right-panel">
-            <SearchBox />
-            <Hits hitComponent={Hit} />
-            <Pagination />
-          </div>
-        </InstantSearch>
-      </div>
-    );
-  }
-}
-
-function Hit(props) {
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
   return (
-    <div>
-      <img
-        src={props.hit.image}
-        align="left"
-        alt={props.hit.name}
-        width={200}
-        height={200}
-        mode="fit"
-      />
-      <div className="hit-title">
-        <Highlight attribute="title" hit={props.hit} />
-      </div>
-      <div className="hit-description">
-        <Highlight attribute="text" hit={props.hit} />
-      </div>
-      <div className="hit-pagetype">{props.hit.pageType}</div>
-      <div className="hit-pagepublicationdate">
-        {props.hit.pagePublicationDate}
-      </div>
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && <Box sx={{ p: 2 }}>{children}</Box>}
     </div>
   );
 }
 
-Hit.propTypes = {
-  hit: PropTypes.object.isRequired,
-};
+export default function App() {
+  const [value, setValue] = React.useState(0);
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+  return (
+    <div className="ais-InstantSearch">
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam tincidunt
+        tellus lorem, sed porttitor neque gravida vitae.
+      </p>
+      <InstantSearch indexName="crawler_tg_content" searchClient={searchClient}>
+        <SearchBox
+          autoFocus
+          translations={{
+            submitTitle: 'Verstuur je zoekopdracht',
+            resetTitle: 'Maak je zoekopdracht leeg',
+            placeholder: 'Zoek in artikelen en koopwijzers',
+          }}
+        />
+        <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+          <Tabs value={value} onChange={handleChange} centered>
+            <Tab label="Artikelen" />
+            <Tab label="Koopwijzer" />
+          </Tabs>
+        </Box>
+        <TabPanel value={value} index={0}>
+          <Index indexName="crawler_tg_content">
+            <Box sx={{ flexGrow: 2 }}>
+              <Grid container spacing={0}>
+                <Grid item xs={12} sm={12} md={3} lg={2} xl={1}>
+                  <Card
+                    sx={{
+                      width: {
+                        xs: 1.0, // 100%
+                        sm: 1.0,
+                        md: 0.95,
+                      },
+                      borderRadius: 2,
+                      bgcolor: '#f2f2f2',
+                      border: 'none',
+                    }}
+                    variant="outlined"
+                  >
+                    <CardContent>
+                      <Typography variant="h5">Categorieën</Typography>
+                      <HierarchicalMenu
+                        attributes={['categories.lvl0', 'categories.lvl1']}
+                      />
+                    </CardContent>
+                  </Card>
+                </Grid>
+                <Grid item xs={12} sm={12} md={9} lg={10} xl={11}>
+                  <Configure hitsPerPage={8} />
+                  <Hits hitComponent={Hit} />
+                  <Pagination />
+                </Grid>
+              </Grid>
+            </Box>
+          </Index>
+        </TabPanel>
 
-export default App;
+        <TabPanel value={value} index={1}>
+          <Index indexName="crawler_tg_koopwijzer">
+            <div className="left-panel">
+              <h2>Categorieën</h2>
+              <HierarchicalMenu
+                attributes={['categories.lvl0', 'categories.lvl1']}
+              />
+            </div>
+            <div className="right-panel">
+              <Hits hitComponent={Hit} />
+              <Pagination />
+            </div>
+          </Index>
+        </TabPanel>
+      </InstantSearch>
+    </div>
+  );
+}
+
+function Hit(props) {
+  return (
+    <Card
+      sx={{
+        width: {
+          xs: 1.0, // 100%
+          sm: 250,
+          md: 300,
+        },
+      }}
+    >
+      <CardMedia
+        component="img"
+        height="240"
+        image={props.hit.image}
+        alt={props.hit.name}
+      />
+      <CardContent>
+        <div className="hit-title">
+          <Highlight attribute="title" hit={props.hit} />
+        </div>
+        <div className="hit-description">
+          <Highlight attribute="text" hit={props.hit} />
+        </div>
+        <div className="hit-pagetype">{props.hit.pageType}</div>
+        <div className="hit-pagepublicationdate">
+          {props.hit.pagePublicationDate}
+        </div>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Lees meer</Button>
+      </CardActions>
+    </Card>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
-import React from 'react';
+import * as React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { StyledEngineProvider } from '@mui/material/styles';
 import App from './App';
+import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <StyledEngineProvider injectFirst>
+    <App />
+  </StyledEngineProvider>,
+  document.getElementById('root')
+);
